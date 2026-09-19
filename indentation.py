c3732068ad5a37637d00c297d203fc5c86fbc1ed
PyGenicArc <145740383+pygenicarc@users.noreply.github.com>
2026-09-19 20:02:51 +0530

    Create indentation.py

---
+++ indentation.py
@@ -0,0 +1,40 @@
+a = 10
+b = 20
+c = 15
+# Check the relationships between the values
+# if a > b:
+#     print("a is greater than b")
+# elif a > c:
+#     print("a is greater than c")
+# else:
+#     print("a is not greater than b or c")
+
+# check multiple conditions
+# if (a > b) and (a > c):
+#     print("a is greater than both b and c")
+# elif (a > b) or (a > c):
+#     print("a is greater than either b or c")
+# else:
+#     print("a is not greater than b or c")
+
+
+# understanding of the if-else statement
+# if <condition>:
+#     # code block to execute if the condition is True
+#     pass
+# else:
+#     # code block to execute if the condition is False
+#     pass
+
+# understanding of the if-elif-else statement
+# if <condition1>:
+#     # code block to execute if condition1 is True
+# elif <condition2>:
+#     # code block to execute if condition2 is True
+# else:
+#     # code block to execute if both condition1 and condition2 are False
+#     pass
+
+if a > b:
+    print("a is greater than b")
+
